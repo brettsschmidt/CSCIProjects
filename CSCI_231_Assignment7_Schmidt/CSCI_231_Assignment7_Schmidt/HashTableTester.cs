81620e503c05a32ed167ef6055dcdad329bd8c9b
Brett Schmidt <brettsschmidt@gmail.com>
2018-02-12 16:40:57 -0500

    Add files via upload
    A strongly-written hash-table that uses a string as a key and can hold any object.

---
+++ CSCI_231_Assignment7_Schmidt/CSCI_231_Assignment7_Schmidt/HashTableTester.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI_231_Assignment7_Schmidt
+{
+    class HashTableTester
+    {
+        public class student
+        {
+            private string studentId;
+            private string name;
+            private string GPA;
+            private string major;
+            private string email;
+
+            public student(string v_studentID)
+            {
+                studentId = v_studentID;
+                name = "Fred";
+                GPA = "1";
+                major = "Liberal Arts";
+                email = "Somethingkool@gmail.com";
+            }
+
+            public string getStudentID()
+            {
+                return studentId;
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            SchmidtsHashTable newHasher = new SchmidtsHashTable(199);
+            LinkedList<student> students = new LinkedList<student>();
+
+            students.AddLast(new student("10056486"));
+            students.AddLast(new student("50098451"));
+            students.AddLast(new student("70056165"));
+            students.AddLast(new student("80856495"));
+            students.AddLast(new student("50056168"));
+            students.AddLast(new student("10056498"));
+            students.AddLast(new student("05686492"));
+            students.AddLast(new student("60058945"));
+            students.AddLast(new student("80058794"));
+            students.AddLast(new student("87948148"));
+            
+            foreach(student c_Student in students)
+            {
+                newHasher.set(c_Student.getStudentID(), c_Student);
+
+            }
+
+            foreach(student c_Student in students)
+            {
+
+                foreach (student h_student in newHasher.get(c_Student.getStudentID()))
+                    {
+                    if(newHasher.get(c_Student.getStudentID()).Length > 1)
+                        {
+                        Console.WriteLine("Collision found with student ID: " + h_student.getStudentID());
+                    }
+                    Console.WriteLine("Pulled student id: " + h_student.getStudentID() + " from the HashTable.");
+                    }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CSCI_231_Assignment7_Schmidt/CSCI_231_Assignment7_Schmidt/SchmidtsHashTable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI_231_Assignment7_Schmidt
+{
+    class SchmidtsHashTable
+    {
+
+        private int m_hashTableSize;
+        private LinkedList<object>[] m_hashTable;
+
+        public SchmidtsHashTable(int hashTableSize)
+        {
+            m_hashTableSize = hashTableSize;
+            m_hashTable = new LinkedList<object>[m_hashTableSize];
+        }
+
+        public void set(string key, object m_value)
+        {
+            int largeInt = 0;
+
+            foreach(byte c in key )
+            {
+                largeInt += largeInt + c;
+                largeInt *= largeInt / c;
+            }
+            if(largeInt < 0)
+            {
+                largeInt = largeInt * -1;
+            }
+
+            int hashKey = largeInt % (m_hashTableSize - 1);
+
+            try
+            {
+                m_hashTable[hashKey].AddLast(m_value);
+            }
+            catch(Exception E)
+            {
+                m_hashTable[hashKey] = new LinkedList<object>();
+                m_hashTable[hashKey].AddLast(m_value);
+            }
+
+        }
+
+        public object[] get(string key)
+        {
+            int largeInt = 0;
+
+            foreach (byte c in key)
+            {
+                largeInt += largeInt + c;
+                largeInt *= largeInt / c;
+            }
+
+            if (largeInt < 0)
+            {
+                largeInt = largeInt * -1;
+            }
+
+            int hashKey = largeInt % (m_hashTableSize - 1);
+
+            return m_hashTable[hashKey].ToArray<object>();
+
+        }
+
+    }
+}
