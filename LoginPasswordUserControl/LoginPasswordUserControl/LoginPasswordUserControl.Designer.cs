d439b9c04c89402ad753c04971c138effadd56ea
Brett Schmidt <brettsschmidt@gmail.com>
2018-02-14 14:57:17 -0500

    Maze Solver and User Control
    LoginPasswordUserControl
    	C# User Control that provides a panel with appropriate settings and textboxes for login and password.
    CSCI280_MazeRunner_Schmidt
    	C# program that takes in a text document with a maze depicted as 1's and 0's (1 representing traversable space) and provides the output of the directions to get out of the maze by modeling the points as Vectors in a tree structure and backtracking out of the tree structure.

---
+++ LoginPasswordUserControl/LoginPasswordUserControl/LoginPasswordUserControl.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace LoginPasswordUserControl
+{
+    partial class LoginPasswordUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(17, 27);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(47, 17);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(17, 80);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(73, 17);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(96, 24);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(145, 22);
+            this.loginTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(96, 80);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(145, 22);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // LoginPasswordUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Name = "LoginPasswordUserControl";
+            this.Size = new System.Drawing.Size(296, 150);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
---
+++ LoginPasswordUserControl/LoginPasswordUserControl/LoginPasswordUserControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPasswordUserControl
+{
+    public partial class LoginPasswordUserControl : UserControl
+    {
+        public LoginPasswordUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public string getLogin()
+        {
+            return loginTextBox.Text;
+        }
+        public string getPassword()
+        {
+            return passwordTextBox.Text;
+        }
+    }
+}
